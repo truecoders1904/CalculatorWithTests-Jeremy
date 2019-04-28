e25f42834acafc8a7ff6f03ef351e0d6ed8b2003
Jeremy <jlong.223.jl@gmail.com>
2019-04-28 17:51:05 -0500

    Added 5 not implemented methods

---
+++ CalculatorWithTests/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorWithTests
+{
+    class Calculator
+    {
+       public int Add(int number1, int number2)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public double Subtract(double minuend,  double subtrahend)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long  Multiply(long factor1,long factor2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Divide(double num1, double num2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int factorial(int num1, int num2, int num3, int num4)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
